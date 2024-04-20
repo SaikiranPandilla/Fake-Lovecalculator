880133ec1813e1651ecc638e9b74922905162e0d
Saikiran Reddy <126289389+SaikiranPandilla@users.noreply.github.com>
2024-04-20 03:37:38 -0500

    Create love.js

---
+++ love.js
@@ -0,0 +1,10 @@
+function love(){
+prompt("Enter the girl name:");
+prompt("Enter boy name:");
+var n = Math.random();
+  n=n*99;
+  n = Math.floor(n) ;
+  alert("you love is "+ n + "%");
+}
+
+love();
